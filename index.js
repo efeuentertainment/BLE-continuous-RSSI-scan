d0c299ceb5fdd71d8f1538877ac9967e0dfb1a35
efeuentertainment <fire@whatimean.ch>
2020-07-05 17:49:36 +0200

    Readme & discover code again

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ noble = require('@abandonware/noble');
 
 let bleRssi = 0;
 //let peripheralIdOrAddress = hard.BLEMAC.toLowerCase();
-let peripheralIdOrAddress = "e6:4e:57:09:74:e4".toLowerCase(); //for debug
+//let peripheralIdOrAddress = "e6:4e:57:09:74:e4".toLowerCase(); //for debug
+const peripheralIdOrAddress = process.argv[2].toLowerCase();
 
 //1. make sure bluetooth is enabled in /boot/config.txt . should look like this:
 //  #dtoverlay=pi3-disable-bt
@@ -10,17 +11,13 @@ let peripheralIdOrAddress = "e6:4e:57:09:74:e4".toLowerCase(); //for debug
 //2. sudo systemctl enable hciuart.service and reboot
 //if those requirements are not met, any usage of noble will throw an error
 
-if (peripheralIdOrAddress === 0) {
+if (peripheralIdOrAddress === "") {
   console.log(`BLE inactive. Bluetooth in /boot/config.txt disabled, hciuart.service not running, or MAC address not set`);
 } else {
   //start BLE scan
   noble.on('stateChange', function (state) {
     if (state === 'poweredOn') {
-      if (peripheralIdOrAddress === 0) {
-        console.log(`BLE inactive. no BLE MAC address set.`);
-      } else {
-        noble.startScanning([], true) //allows duplicates while scanning
-      }
+      noble.startScanning([], true) //allows duplicates while scanning
     } else {
       noble.stopScanning();
     }
